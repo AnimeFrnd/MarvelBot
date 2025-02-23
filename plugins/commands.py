3a9ac32a75443fd36f942851e74883c403abae20
AnimeFrnd <163717030+AnimeFrnd@users.noreply.github.com>
2025-02-23 09:06:44 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -17,9 +17,7 @@ from database.connections_mdb import active_connection
 from urllib.parse import quote_plus
 from TechVJ.util.file_properties import get_name, get_hash, get_media_file_size
 logger = logging.getLogger(__name__)
-
     return btn
-
 BATCH_FILES = {}
 join_db = JoinReqs
 
